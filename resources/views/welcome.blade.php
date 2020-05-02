c3130873984801120872fa4b3c6cd99d9b2bcd91
githantonio <ghitantonio@gmail.com>
2020-05-02 22:03:06 +0100

    Adicionadno tino

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 
+                    Laravel Tino
                 </div>
 
                 <div class="links">
