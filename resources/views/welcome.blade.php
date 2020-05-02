979cb3b30e2d5b216bfa095511d51650d8974eb4
githantonio <ghitantonio@gmail.com>
2020-05-02 19:31:52 +0100

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Tino Aula 1
+                    Laravel 
                 </div>
 
                 <div class="links">
